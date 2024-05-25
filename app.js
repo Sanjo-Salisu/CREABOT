f2a2fa99c7f63560cf548d1c14e6281e10ff52f3
Sanjo <sanjosalisuskillz@gmail.com>
2024-05-25 08:17:38 -0700

    server

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-console.log("Hello world")
-console.log("Hello World")
-CONSOLE.LOG("Page Interface already interlocked")
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const port = process.env.port
 
+console.log("You're doing well")
+app.listen(port, () => console.log (`Server started on http://localhosst:${port}`))
